b9f58fe93adc0b104aaf8484cc019e0a058d272b
cmmarques22 <carlos.marques22@live.com>
2023-11-28 17:10:17 +0000

    test commit

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int homem = 0;
+        int mulher = 0;
+
+        Console.WriteLine($"Before Increment: Value1 = {homem}, Value2 = {mulher}");
+
+        // Use a for loop to repeatedly read input and increment values
+        for (int i = 0; i < 5; i++) // Change the loop condition as needed
+        {
+            Console.Write("Enter 'H' or 'M': ");
+            
+            string input = Console.ReadLine();
+            if (input == "H")
+            {
+                homem++;
+                Console.WriteLine($"Homem:{homem} Mulher: {mulher} ");
+            }
+            else if (input == "M")
+            {
+                mulher++;
+                Console.WriteLine($"Homem:{homem} Mulher: {mulher} ");
+            }
+            else
+            {
+                Console.WriteLine("H ou M, não é fisica quantica");
+            }
+
+
+        }
+
+        Console.WriteLine($"Resultado Final = {homem}, Value2 = {mulher}");
+    }
+
+    
+    }
+
+
