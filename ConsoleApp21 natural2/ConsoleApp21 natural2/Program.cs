25fce2e653f245aa7a1397f884bf37dcdebd0fe0
cmmarques22 <carlos.marques22@live.com>
2023-12-08 12:51:57 +0000

    whiles whiles whiles

---
+++ ConsoleApp21 natural2/ConsoleApp21 natural2/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("teste numeros inteiros, filtrar media e primos");
+
+
+Console.WriteLine("Insira um numero inteiro maior que 1");
+int input = 0;
+if (input < 2)
+{
+    Console.WriteLine("numero invalido, tente outra vez");
+    input = int.Parse(Console.ReadLine());
+
+}
+
+int primeSum = 0;
+int primeCounter = 0;
+
+
+while (true)
+{
+    //verificar primo
+    input = int.Parse(Console.ReadLine());
+    
+    if (input % 2 == 0)
+    {
+
+        //verificar intervalo
+        if (input >= 100 && input <= 200)
+        {
+            //contar numeros primos
+            primeCounter++;
+
+
+
+            //fazer soma dos numeros no intervalo   
+            primeSum += primeCounter;
+            int primeAverage = primeSum / primeCounter;
+
+        }
+    }
+    Console.WriteLine($"A soma é de {primeSum}");
+    if (primeCounter > 0)
+    {
+        double primeAverage = (double)primeSum / primeCounter;
+        Console.WriteLine($"A média de números primos é de {primeAverage}");
+    }
+    Console.WriteLine($" a soma é de {primeSum} ");
+
+    // Pedir novo input
+    Console.WriteLine("Insira outro número inteiro ou digite '0' para sair");
+    input = int.Parse(Console.ReadLine());
+
+    // Sair do loop se o input for 0
+    if (input == 0)
+    {
+        break;
+    }
+}
+
---
+++ ConsoleApp22 - primeNumber/ConsoleApp22 - primeNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿
+Console.WriteLine("teste numeros inteiros, filtrar media e primos");
+
+
+Console.WriteLine("Insira um numero inteiro maior que 1");
+int input = int.Parse(Console.ReadLine());
+if (input < 2)
+{
+    Console.WriteLine("numero invalido, tente outra vez"); 
+    input = int.Parse(Console.ReadLine());
+
+}
+
+int primeSum = 0;
+int primeCounter =0;
+
+
+while (true)
+{
+    //verificar primo
+    if (
+        (input < 2) 
+        &&  
+        (input % 2 == 0))
+    {
+        
+        
+        //verificar intervalo
+        if (input <= 100 && input >= 200)
+        { 
+            //contar numeros primos
+            primeCounter++;
+            
+            
+            
+            //fazer soma dos numeros no intervalo   
+            primeSum += primeCounter;
+        
+        } 
+    }
+}
+double primeAverage = primeSum/primeCounter ;
+Console.WriteLine(" a soma é de {primeSum} " + "a media de numeros primos é de {primeAverage}");
+
