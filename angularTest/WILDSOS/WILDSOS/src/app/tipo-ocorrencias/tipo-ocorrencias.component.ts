478aa0dff5882b8687743c72d91292235d70157a
cmmarques22 <carlos.marques22@live.com>
2024-02-13 16:57:58 +0000

    sos button create TPO

--- angularTest/WILDSOS/WILDSOS/src/app/tipo-ocorrencias/tipo-ocorrencias.component.ts
+++ angularTest/WILDSOS/WILDSOS/src/app/tipo-ocorrencias/tipo-ocorrencias.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit  } from '@angular/core';
+import { TipoOcorrenciaService } from './tipo-ocorrencia.service';
+import { TipoOcorrencia } from './tipo-ocorrencia';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tipo-ocorrencias',
@@ -7,32 +10,63 @@ import { Component } from '@angular/core';
 })
 export class TipoOcorrenciasComponent {
 
-  currentComponent: string = 'none';
+//   currentComponent: string = 'none';
 
-  openComponent(hyperLink: any) {
-
-
-    if(hyperLink) {
-      if(hyperLink.id == "list") {
-        this.currentComponent = "list-tipo-ocorrencias";
-      }
-      else if(hyperLink.id == "create") {
-        this.currentComponent = "create-tipo-ocorrencia";
-      }
-      else if(hyperLink.id == "update") {
-        this.currentComponent = "update-tipo-ocorrencia";
-      }
-      else if(hyperLink.id == "read") {
-        this.currentComponent = "read-tipo-ocorrencia";
-      }
-      else if(hyperLink.id == "delete") {
-        this.currentComponent = "delete-tipo-ocorrencia";
-      }
-      else {
-        this.currentComponent = '';
-      }
-        }}
+//   openComponent(hyperLink: any) {
 
 
+//     if(hyperLink) {
+//       if(hyperLink.id == "list") {
+//         this.currentComponent = "list-tipo-ocorrencias";
+//       }
+//       else if(hyperLink.id == "create") {
+//         this.currentComponent = "create-tipo-ocorrencia";
+//       }
+//       else if(hyperLink.id == "update") {
+//         this.currentComponent = "update-tipo-ocorrencia";
+//       }
+//       else if(hyperLink.id == "read") {
+//         this.currentComponent = "read-tipo-ocorrencia";
+//       }
+//       else if(hyperLink.id == "delete") {
+//         this.currentComponent = "delete-tipo-ocorrencia";
+//       }
+//       else {
+//         this.currentComponent = '';
+//       }
+//         }}
+currentComponent: string = 'none';
+
+openComponent(hyperLink: any) {
+
+
+  if(hyperLink) {
+  
+     if(hyperLink.id == "create") {
+      this.currentComponent = "create-tipo-ocorrencia";
+    }
+   
+    else {
+      this.currentComponent = '';
+     }
+      }}
+  
+    tipoOcorrenciaList: TipoOcorrencia[] = [];
+  tipoOcorrencia?: TipoOcorrencia = undefined;
+  constructor(private service: TipoOcorrenciaService) {
+  }
+
+    ngOnInit(): void {
+      this.service.getAll().then((data: Observable<TipoOcorrencia[]>) => {
+       //o then recebe um retorn subscribe para tirar a lista de familas por causa da promise
+       // so é preciso fazer o subscribe
+       data.subscribe(list => {
+         console.log(list);
+         this.tipoOcorrenciaList = list;
+       });
+      }).catch((error: any) => {
+       console.log(error);
+      });
+     }
 
 }
